f3a395614cea239aaa96c6b7c456a97aeb7eaed1
SnowBit <snowbitcoderboi@gmail.com>
2021-12-31 15:32:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// Discord
-console.log('%cHold Up', 'color: #404EED; font-size: 60px; font-weight: bold;-webkit-text-stroke: 1px black;');
-console.log("%cIf someone told you to copy/paste something here you have an 11/10 chance you're being scammed", "color: black; font-size: 18px;")
-console.log("%cPasting anything in here could give attackers access to your Discord account.", "color: red; font-size: 18px; font-weight: bold;")
+console.log('%cSubscribe Now', 'color: #404EED; font-size: 60px; font-weight: bold;-webkit-text-stroke: 1px black;');
+console.log("%chttps://www.youtube.com/channel/UCNTKqF1vhFYX_v0ERnUa1RQ?view_as=subscriber&sub_confirmation=1", "color: black; font-size: 18px;")
 
-// FaceBook
-console.log("%cStop!", "color: red; font-size: 50px;font-weight: bold; font-family: sans-serif;-webkit-text-stroke: 1px black;");
-
-console.log("%cThis is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Facebook feature or hack someone's account, it is a scam and will give them access to your Facebook account", "font-size:20px; font-family: sans-serif;");
-
-console.log("%cSee https://www.facebook.com/selfxss for more information.", "font-size:20px; font-family: sans-serif;")

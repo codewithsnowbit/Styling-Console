3822a284b8b166607e3f04245c6d9829fb158789
SnowBit <dtshah24022006@gmail.com>
2021-12-31 12:10:19 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Discord
+console.log('%cHold Up', 'color: #404EED; font-size: 60px; font-weight: bold;-webkit-text-stroke: 1px black;');
+console.log("%cIf someone told you to copy/paste something here you have an 11/10 chance you're being scammed", "color: black; font-size: 18px;")
+console.log("%cPasting anything in here could give attackers access to your Discord account.", "color: red; font-size: 18px; font-weight: bold;")
+
+// FaceBook
+console.log("%cStop!", "color: red; font-size: 50px;font-weight: bold; font-family: sans-serif;-webkit-text-stroke: 1px black;");
+
+console.log("%cThis is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Facebook feature or hack someone's account, it is a scam and will give them access to your Facebook account", "font-size:20px; font-family: sans-serif;");
+
+console.log("%cSee https://www.facebook.com/selfxss for more information.", "font-size:20px; font-family: sans-serif;")
